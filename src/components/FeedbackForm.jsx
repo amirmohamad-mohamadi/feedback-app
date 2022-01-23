e6540402b039310699c513a86dedd4399b6aff5d
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-23 22:22:39 +0330

    feedbackForm validation

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from "./shared/Button";
 
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
 
   const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("طول جمله باید بیشتر از ده کاراکتر باشد");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
     setText(e.target.value);
   };
 
@@ -20,8 +32,11 @@ const FeedbackForm = () => {
           placeholder="Write a review"
           value={text}
         />
-        <Button type="submit">send</Button>
+        <Button type="submit" isDiabled={btnDisabled}>
+          send
+        </Button>
       </div>
+      {message && <div className="message">{message}</div>}
     </Card>
   );
 };
