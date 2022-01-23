064fdbb82af06ca481fef0cc75c3aed870c338c6
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-24 00:58:55 +0330

    install framer-motion and use in feedbackList

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from "framer-motion";
 import PropTypes from "prop-types";
 
 import FeedbackItem from "./FeedbackItem";
@@ -6,16 +7,41 @@ const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>Not Feedback Yet</p>;
   }
+
   return (
     <div>
-      {feedback.map((item) => {
-        return (
-          <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-        );
-      })}
+      <AnimatePresence>
+        {feedback.map((item) => {
+          return (
+            <motion.div
+              key={item.id}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            >
+              <FeedbackItem
+                key={item.id}
+                item={item}
+                handleDelete={handleDelete}
+              />
+            </motion.div>
+          );
+        })}
+      </AnimatePresence>
     </div>
   );
 };
+
+//   return (
+//     <div>
+//       {feedback.map((item) => {
+//         return (
+//           <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
