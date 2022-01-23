1aaf0db27a7209c32a4a8b329e5907888f3a1cb5
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-23 18:43:21 +0330

    create feedbackStats for review and average-rating

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,12 @@
+const FeedbackStats = ({ feedback }) => {
+  let average =
+    feedback.reduce((acc, item) => acc + item.rating, 0) / feedback.length;
+  return (
+    <div className="feedback-stats">
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+    </div>
+  );
+};
+
+export default FeedbackStats;
