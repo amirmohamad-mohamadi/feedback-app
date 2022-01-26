77104c5be22774dcae477e663346224d0dc9cc6d
amirmohamad-mohamadi <76755078+amirmohamad-mohamadi@users.noreply.github.com>
2022-01-26 11:45:42 +0330

    install json-server and concurrently. show information from json-server with fetch

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,12 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
-
-import FeedbackData from "../data/FeedbackData";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState([]);
+
+  useEffect(() => {
+    fetchFeedback();
+  }, []);
+
+  const fetchFeedback = async () => {
+    const response = await fetch("http://localhost:5000/feedback");
+    const data = await response.json();
+    setFeedback(data);
+  };
+
   const [editFeedback, setEditFeedback] = useState({
     item: {},
     edit: false,
